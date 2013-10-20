73a1c22a584ef0e0e57a8199cd3dc2c2f3b2f7a0
Alex Paul <axp_alpaul@yahoo.com>
2013-10-20 07:34:10 -0400

    added a test class

---
+++ Git Test.h
@@ -0,0 +1,13 @@
+//
+//  Git Test.h
+//  Git Test
+//
+//  Created by Alex Paul on 10/20/13.
+//  Copyright (c) 2013 Alex Paul. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Git_Test : NSObject
+
+@end
---
+++ Git Test.m
@@ -0,0 +1,13 @@
+//
+//  Git Test.m
+//  Git Test
+//
+//  Created by Alex Paul on 10/20/13.
+//  Copyright (c) 2013 Alex Paul. All rights reserved.
+//
+
+#import "Git Test.h"
+
+@implementation Git_Test
+
+@end
